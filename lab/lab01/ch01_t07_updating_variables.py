2837a46a95dd3095e164594d48c9c7732a47db65
Lucasli-2026 <kingdomjp2016@gmail.com>
2026-09-03 09:30:54 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -11,7 +11,7 @@ september_rainfall = 5.16
 annual_rainfall += september_rainfall
 
 october_rainfall = 7.20
-annual_rainfall += september_rainfall
+annual_rainfall += october_rainfall
 
 november_rainfall = 5.06
 annual_rainfall += september_rainfall
